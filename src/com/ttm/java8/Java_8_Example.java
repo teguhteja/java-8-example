cebdf1cecc466f48a7901a0f217184b26c109ab4
IB Teguh Teja Murti <teguh.teja@gmail.com>
2017-07-23 11:49:01 +0800

    new file:   build.xml 	new file:   build/classes/.netbeans_automatic_build 	new file:   build/classes/.netbeans_update_resources 	new file:   build/classes/com/ttm/java8/Java_8_Example.class 	new file:   manifest.mf 	new file:   nbproject/build-impl.xml 	new file:   nbproject/genfiles.properties 	new file:   nbproject/private/private.properties 	new file:   nbproject/project.properties 	new file:   nbproject/project.xml 	new file:   src/com/ttm/java8/Java_8_Example.java

---
+++ src/com/ttm/java8/Java_8_Example.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ttm.java8;
+
+/**
+ *
+ * @author palgunadi
+ */
+public class Java_8_Example {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+    
+}
